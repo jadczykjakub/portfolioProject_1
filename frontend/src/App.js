5a7664f3e92b2986e3f9b33572e3b9c7ab067746
jadczykajakub <jadczyk.jakub@gmail.com>
2023-10-31 20:10:58 +0100

    husky check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-
-
-
-
-
-
-      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
