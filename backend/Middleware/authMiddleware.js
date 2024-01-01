c0887efe0d98a2d73dd72186ecc581a911627bb9
jadczykajakub <jadczyk.jakub@gmail.com>
2024-01-01 21:12:05 +0100

    typo

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -1,4 +1,3 @@
-
 import jwt from "jsonwebtoken";
 
 export const checkUser = (req, res, next) => {
@@ -11,6 +10,7 @@ export const checkUser = (req, res, next) => {
       console.log("elo");
       if (err) {
         console.log("elo3");
+        console.log(res);
         // res.redirect("/dupa");
 
         //TODO what middleware do when not correct.
